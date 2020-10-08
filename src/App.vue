6399c666ac99006ded4cf3b8941eedc372402d8c
violateer <1828257089@qq.com>
2020-10-08 16:06:35 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
+<div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="我是铁蛋儿" />
-  </div>
+    <HelloWorld msg="vio-cli" />
+</div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+    name: "app",
+    components: {
+        HelloWorld
+    }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
